53037d0e752eabb2349f8c5bbce2b9c0ba8be5ac
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2016-11-16 16:11:26 +0100

    Added copyright information to header

--- qualikiz_tools/qualikiz_io/analyse.py
+++ qualikiz_tools/qualikiz_io/analyse.py
@@ -1,3 +1,8 @@
+"""
+Copyright Dutch Institute for Fundamental Energy Research (2016)
+Contributors: Karel van de Plassche (karelvandeplassche@gmail.com)
+License: CeCILL v2.1
+"""
 import xarray as xr
 import pandas as pd
 import matplotlib.pyplot as plt
