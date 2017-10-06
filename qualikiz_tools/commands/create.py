894dc534fe6623872ebfa26942babf24ea88d5d8
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2017-10-06 09:37:26 +0200

    Added command to create QuaLiKizRun from JSON

--- qualikiz_tools/commands/create.py
+++ qualikiz_tools/commands/create.py
@@ -1,11 +1,12 @@
 """
 Usage: 
-  qualikiz_tools create <target> [<target_path>]
-  qualikiz_tools [-v | -vv] create <target> [<target_path>]
+  qualikiz_tools [-v | -vv] create [--in_dir <directory>] [--binary_dir <directory>] <target> [<parameter_json>]
 
 Options:
+    --in_dir <directory>              Create folder in this folder [default: runs]
+    --binary_dir <directory>          Path to the QuaLiKiz binary [default: ./QuaLiKiz]
   -h --help                         Show this screen.
-  [-v | -vv]                        Verbosity 
+  [-v | -vv]                        Verbosity
 
 Often used commands:
   qualikiz_tools create mini
@@ -32,20 +33,32 @@ def run(args):
         print ('create received:')
         print (args)
         print ()
-    
-    if args['<target_path>']:
-        args['<target_path>'] = os.path.abspath(args['<target_path>'])
 
+    parent_dir = os.path.abspath(args['--in_dir'])
     if args['<target>'] == 'mini':
-        if args['<target_path>']:
-            create_mini(args['<target_path>'])
-        else:
-            create_mini(os.path.abspath('runs'))
+        create_mini(parent_dir)
     elif args['<target>'] == 'performance':
-        if args['<target_path>']:
-            create_performance(args['<target_path>'])
+        create_performance(parent_dir)
+    elif args['<target>'] == 'from_json':
+        json_path = args['<parameter_json>']
+        if json_path is None:
+            raise Exception("Please supply a path to a JSON file. See 'qualikiz_tools create help'")
+        if os.path.isfile(json_path):
+            from qualikiz_tools.qualikiz_io.inputfiles import QuaLiKizPlan
+            plan = QuaLiKizPlan.from_json(json_path)
         else:
-            create_performance(os.path.abspath('runs'))
+            raise Exception('`{!s}` is not a valid JSON file'.format(json_path))
+
+        from qualikiz_tools.qualikiz_io.qualikizrun import QuaLiKizRun
+        name = os.path.basename(json_path.split('.')[0])
+        reldir = os.path.relpath(args['--binary_dir'],
+                                 start=os.path.join(parent_dir, name))
+        if args['-v'] >= 1:
+            verbose = True
+        else:
+            verbose = False
+        run = QuaLiKizRun(parent_dir, name, reldir, verbose=verbose)
+        run.prepare()
     elif args['<target>'] in ['help', None]:
         exit(call([sys.executable, os.path.join(ROOT, 'commands', 'create.py'), '--help']))
     else:
