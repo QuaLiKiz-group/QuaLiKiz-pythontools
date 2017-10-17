6806e267e16c03109f1c9ef7396d1166858c90b8
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2017-10-17 14:02:18 +0200

    Removed class-in-class for System and Bash

--- qualikiz_tools/machine_specific/system.py
+++ qualikiz_tools/machine_specific/system.py
@@ -6,37 +6,37 @@ License: CeCILL v2.1
 from ..qualikiz_io.qualikizrun import QuaLiKizRun, QuaLiKizBatch
 from warnings import warn
 
-class System():
-    class Run(QuaLiKizRun):
-        def to_batch_string(self, *args, **kwargs):
-            raise NotImplementedError('Run to_batch_string not implemented yet')
+class Run(QuaLiKizRun):
+    def to_batch_string(self, *args, **kwargs):
+        raise NotImplementedError('Run to_batch_string not implemented yet')
 
-        @classmethod
-        def from_batch_string(cls, *args, **kwargs):
-            raise NotImplementedError('Run from_batch_string not implemented yet')
+    @classmethod
+    def from_batch_string(cls, *args, **kwargs):
+        raise NotImplementedError('Run from_batch_string not implemented yet')
 
-        @classmethod
-        def from_dir(cls, dir, *args, **kwargs):
-            warn('Specialized from_dir method not defined')
-            return super().from_dir(dir, *args, **kwargs)
+    @classmethod
+    def from_dir(cls, dir, *args, **kwargs):
+        warn('Specialized from_dir method not defined')
+        return super().from_dir(dir, *args, **kwargs)
 
 
-        def launch(self, *args, **kwargs):
-            raise NotImplementedError('Run launch not implemented yet')
+    def launch(self, *args, **kwargs):
+        raise NotImplementedError('Run launch not implemented yet')
 
-    class Batch(QuaLiKizBatch):
-        def to_batch_file(self, path, *args, **kwargs):
-            raise NotImplementedError('Batch to_file not implemented yet')
+class Batch(QuaLiKizBatch):
+    run_class = None
+    def to_batch_file(self, path, *args, **kwargs):
+        raise NotImplementedError('Batch to_file not implemented yet')
 
-        @classmethod
-        def from_batch_file(cls, path, *args, **kwargs):
-            raise NotImplementedError('Batch from_file not implemented yet')
+    @classmethod
+    def from_batch_file(cls, path, *args, **kwargs):
+        raise NotImplementedError('Batch from_file not implemented yet')
 
-        @classmethod
-        def from_dir(cls, dir, *args, **kwargs):
-            warn('Specialized from_dir method not defined')
-            return super().from_subdirs(dir, *args, **kwargs)
+    @classmethod
+    def from_dir(cls, dir, *args, **kwargs):
+        warn('Specialized from_dir method not defined')
+        return super().from_subdirs(dir, *args, **kwargs)
 
-        def launch(self, *args, **kwargs):
-            raise NotImplementedError('Batch launch not implemented yet')
+    def launch(self, *args, **kwargs):
+        raise NotImplementedError('Batch launch not implemented yet')
 
